63a1cdc569a4a54333af5efd6a48042e4d3c42f6
DESKTOP-64APVNH\QUOC <quocitspkt@gmail.com>
2020-12-06 13:50:36 +0700

    The first commit

---
+++ MarketPlace.Infrastructure/SharedKernel/DomainEntity.cs
@@ -0,0 +1,13 @@
+﻿namespace MarketPlace.Infrastructure.SharedKernel
+{
+    public class DomainEntity<T>
+    {
+        public T Id { get; set; }
+
+        //True if domain Entity has an identity
+        public bool IsTransient()
+        {
+            return Id.Equals(default(T));
+        }
+    }
+}
